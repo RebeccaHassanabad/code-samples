1f8e75aafcb874b016c4ade15434e1c79ec9ee69
RebeccaHassanabad <hassanbeck@gmail.com>
2019-04-28 21:59:35 -0600

    Added comment, changed names

---
+++ namesarray.rb
@@ -0,0 +1,16 @@
+#Makes arrays with names and animals
+
+names = ["Ada", "Belle", "John"]
+puts names
+puts names [1]
+puts names [0]
+puts names [2]
+
+puts "Becky" + " Hassanabad"
+
+ary = Array.new(1) {15}
+	puts ary 
+
+animals = ['bear', 'racoon', 'peacock', 'kangaroo', 'whale', 'platypus']
+puts animals [3]
+puts animals [5]
