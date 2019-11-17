e443abc04e38e9e67b60e5aebd429a4f31f8bfee
RebeccaHassanabad <hassanbeck@gmail.com>
2019-11-17 10:46:59 -0700

    adding python scripts

---
+++ pyvariables.py
@@ -0,0 +1,35 @@
+#pyvariables.py
+
+a, b, c = 5, 3.2, "Hello"
+
+print(a)
+print(b)
+print(c)
+
+for i in range(1,11):
+	print(i)
+	if i == 10: 
+		break
+
+if True: 
+	print("Hello")
+	a = 5
+
+def double(num):
+	"""Function to double the value"""
+	return2*num
+	
+	strings = "This is Python"
+	char = "C"
+	multiline_str = """This is a multiline string with more than one line code."""
+	#unicode = u\"u00dcnic\u00f6de"
+	raw_str = r"raw\n string"
+
+	print(strings)
+	print(char)
+	print(multiline_str)
+	print(unicode)
+	print(raw_str)
+
+
+	
